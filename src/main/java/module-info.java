6800f78a9d3ce29384555b45825179a939db18aa
Michał Pajda <gasosaur@gmail.com>
2022-11-21 14:51:19 +0100

    version downgrade

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,10 +1,6 @@
 module com.example.cardgame {
     requires javafx.controls;
-    requires javafx.fxml;
 
-
-    opens com.example.cardgame to javafx.fxml;
     exports com.example.cardgame;
     exports com.example.cardgame.core;
-    opens com.example.cardgame.core to javafx.fxml;
 }
